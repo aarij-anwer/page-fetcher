d621f159b691716576a7c2298732be0aae302931
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-22 23:35:01 -0500

    add the edge cases

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,24 @@
 const request = require('request');
 const fs = require('fs');
+const { access, constants } = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const writeToFile = function(file,body) {
+  fs.writeFile(file, body, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Downloaded and saved ${body.length} bytes to ${file}`);
+    }
+  });
+  rl.close();
+};
 
 const fetcher = function() {
   let URL = process.argv[2];
@@ -8,12 +27,17 @@ const fetcher = function() {
   if ((URL) && (file)) {
     request(URL, (error, response, body) => {
       if ((!error) && response.statusCode === 200) {
-        // do something
-        fs.writeFile(file, body, err => {
-          if (err) {
-            console.log(err);
+        access(file, constants.F_OK, (fileDoesNotExist) => {
+          if (fileDoesNotExist) {
+            writeToFile(file,body);
           } else {
-            console.log(`Downloaded and saved ${body.length} bytes to ${file}`);
+            rl.question('File exists. Override? Y/N ', (answer) => {
+              // do something
+              if (answer === "Y") {
+                writeToFile(file,body);
+              }
+              rl.close();
+            });
           }
         });
       } else {
