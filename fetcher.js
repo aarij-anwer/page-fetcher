dd4bc7710aa81a6406476bbccfa1b6764c7925ab
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-22 23:22:17 -0500

    add request and fs.writeFile to fetcher

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,25 @@
+const request = require('request');
+const fs = require('fs');
+
 const fetcher = function() {
   let URL = process.argv[2];
   let file = process.argv[3];
 
   if ((URL) && (file)) {
-    console.log(URL);
-    console.log(file);
-
-
+    request(URL, (error, response, body) => {
+      if ((!error) && response.statusCode === 200) {
+        // do something
+        fs.writeFile(file, body, err => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`Downloaded and saved ${body.length} bytes to ${file}`);
+          }
+        });
+      } else {
+        console.log("Invalid URL: ", URL, error.code);
+      }
+    });
   } else {
     if (!URL) {
       console.log("Provide URL and file path");
